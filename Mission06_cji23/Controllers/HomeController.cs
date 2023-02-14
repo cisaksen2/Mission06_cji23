d1432c073f47115af23e4eba05b4ff5930e0faa9
cisaksen2 <cji23@byu.edu>
2023-02-13 17:50:25 -0700

    Initial Creation

---
+++ Mission06_cji23/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_cji23.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_cji23.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private AddMovieContext blahContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, AddMovieContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(AddMovie ar)
+        {
+            blahContext.Add(ar);
+            blahContext.SaveChanges();
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_cji23/Models/AddMovieContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_cji23.Models
+{
+    public class AddMovieContext : DbContext
+    {
+        public AddMovieContext(DbContextOptions<AddMovieContext> options) : base(options)
+        {
+            // leave blank for now
+        }
+
+        public DbSet<AddMovie> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<AddMovie>().HasData(
+
+                new AddMovie
+                {
+                    MovieId = 1,
+                    Category = "Action/Adventure",
+                    Title = "Avengers, The",
+                    Year = 2012,
+                    Director = "Josh Whedon",
+                    Rating = "PG-13", 
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+                new AddMovie
+                {
+                    MovieId = 2,
+                    Category = "Action/Adventure",
+                    Title = "Indiana Jones and the Last Crusade",
+                    Year = 1989,
+                    Director = "Steven Spielberg",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+                new AddMovie
+                {
+                    MovieId = 3,
+                    Category = "Action/Adventure",
+                    Title = "Pirates of the Caribbean: The Curse of the Black Pearl",
+                    Year = 2003,
+                    Director = "Gore Verbinski",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                }
+            );
+        }
+    }
+}
